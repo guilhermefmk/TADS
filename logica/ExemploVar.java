5a199b442c9ecd6c1b382f2748efaa90ee958538
guilhermefmk <guilhermefmk9@gmail.com>
2022-08-10 19:22:04 -0300

    Teste 2

--- logica/ExemploVar.java
+++ logica/ExemploVar.java
@@ -8,7 +8,6 @@ public class ExemploVar {
        float av2 = in.nextFloat();
        float av3 = in.nextFloat();
        float trab = in.nextFloat();
-
        float media = (av1 + av2 + av3 + trab)/4;
        System.out.printf("Sua média foi %.2f\n",media);
         
